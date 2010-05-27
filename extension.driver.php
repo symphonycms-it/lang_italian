749f003d71d644031397b5adb2f61a049df30495
Simone Economo <my.ekoes@gmail.com>
2010-05-28 00:33:25 +0200

    Fix to issue #1 (reported by pngised)

--- extension.driver.php
+++ extension.driver.php
@@ -9,25 +9,25 @@
 		public function about() {
 			return array(
 				'name' => 'Language: Italiano',
-				'type' => 'Localisation',
-				'version' => '1.1',
-				'release-date' => '2010-02-25',
+#				'type' => 'Localisation',
+				'version' => '1.1.1',
+				'release-date' => '2010-05-28',
 				'author' => array(
 					'name' => 'Simone Economo',
 					'website' => 'http://lineheight.net',
 					'email' => 'my.ekoes@gmail.com'
 				),
 				'description' => 'Official Italian translation for the Symphony backend',
-				'compatibility' => array(
-					'2.0.0' => false,
-					'2.0.1' => false,
-					'2.0.2' => false,
-					'2.0.3' => true,
-					'2.0.4' => true,
-					'2.0.5' => true,
-					'2.0.6' => true,
-					'2.0.7' => true
-				)
+#				'compatibility' => array(
+#					'2.0.0' => false,
+#					'2.0.1' => false,
+#					'2.0.2' => false,
+#					'2.0.3' => true,
+#					'2.0.4' => true,
+#					'2.0.5' => true,
+#					'2.0.6' => true,
+#					'2.0.7' => true
+#				)
 			);
 		}
 
